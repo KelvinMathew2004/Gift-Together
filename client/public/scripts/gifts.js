1760357e5ba26404557ccf074e717034d491cc0a
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-09-20 12:21:53 -0500

    Step 10: Set up 404 page

--- client/public/scripts/gifts.js
+++ client/public/scripts/gifts.js
@@ -49,34 +49,11 @@ const renderGifts = async () => {
     }
 }
 
-renderGifts()
+const requestedUrl = window.location.href.split('/').pop()
 
-const renderGift = async () => {
-    const requestedID = parseInt(window.location.href.split('/').pop())
-    const response = await fetch('/gifts')
-    const data = await response.json()
-
-    const giftContent = document.getElementById('gift-content')
-    let gift
-
-    if (data) {
-        gift = data.find(gift => gift.id === requestedID)
-    }
-
-    if (gift) {
-        document.getElementById('image').src = gift.image
-        document.getElementById('name').textContent = gift.name
-        document.getElementById('submittedBy').textContent = 'Submitted by: ' + gift.submittedBy
-        document.getElementById('pricePoint').textContent = 'Price: ' + gift.pricePoint
-        document.getElementById('audience').textContent = 'Great For: ' + gift.audience
-        document.getElementById('description').textContent = gift.description
-        document.title = `UnEarthed - ${gift.name}`
-    }
-    else {
-        const message = document.createElement('h2')
-        message.textContent = 'No Gifts Available 😞'
-        giftContent.appendChild(message)
-    }
+if (requestedUrl) {
+    window.location.href = '../404.html'
 }
-
-renderGift()
+else {
+    renderGifts()
+}
