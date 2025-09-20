6595ffe76c46ca70803103a4627d174fb9a6b6e6
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-09-20 13:05:37 -0500

    Styling improvements for header

--- server/public/scripts/header.js
+++ server/public/scripts/header.js
@@ -10,7 +10,7 @@ const headerLogo = document.createElement('img')
 headerLogo.src = '/logo.png'
 
 const headerTitle = document.createElement('h1')
-headerTitle.textContent = 'UnEarthed'
+headerTitle.textContent = 'GiftTogether'
 
 headerLeft.appendChild(headerLogo)
 headerLeft.appendChild(headerTitle)
@@ -18,7 +18,7 @@ headerLeft.appendChild(headerTitle)
 const headerRight = document.createElement('div')
 headerRight.className = 'header-right'
 
-const headerButton = document.createElement('Home')
+const headerButton = document.createElement('button')
 headerButton.textContent = 'Home'
     
 headerButton.addEventListener('click', function handleClick(event) {
